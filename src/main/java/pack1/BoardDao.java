45d1ad01aec03a309afe5a72fa7fe44854d3edda
Carpeii <52017539+Carpeii@users.noreply.github.com>
2024-06-20 10:37:53 +0900

    dao, dto 추가

---
+++ src/main/java/pack1/BoardDao.java
@@ -0,0 +1,31 @@
+package pack1;
+
+public class BoardDao {
+    public BoardDao(){
+        
+    }
+    public void boardWrite(){
+
+    }
+    public void boardWriteOk(){
+
+    }
+    public void boardList(){
+
+    }
+    public  void boardView(){
+
+    }
+    public void boardDelete(){
+
+    }
+    public void boardDeleteOk(){
+
+    }
+    public void boardModify(){
+
+    }
+    public void boardModifyOk(){
+
+    }
+}
